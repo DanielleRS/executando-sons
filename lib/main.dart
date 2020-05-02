dde85617f7c3994b4d4ef68cc0279c92bcfbf27e
Danielle <daniellerodris@gmail.com>
2020-05-02 20:00:28 -0300

    Pausando e parando

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,16 @@ class _HomeState extends State<Home> {
 
   AudioPlayer audioPlayer = AudioPlayer();
   AudioCache audioCache = AudioCache(prefix: "audios/");
+  bool firstRun = true;
 
   _play() async {
-    audioPlayer = await audioCache.play("musica.mp3");
+    if(firstRun){
+      audioPlayer = await audioCache.play("musica.mp3");
+      firstRun = false;
+    } else{
+      audioPlayer.resume();
+    }
+
     /*
     String url = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3";
     int result = await audioPlayer.play(url);
@@ -27,9 +34,61 @@ class _HomeState extends State<Home> {
     */
   }
 
+  _pause() async {
+    int result = await audioPlayer.pause();
+    if(result == 1) {
+
+    }
+  }
+
+  _stop() async {
+    int result = await audioPlayer.stop();
+    if(result == 1) {
+
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    _play();
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text("Executando sons"),),
+      body: Column(
+        children: <Widget>[
+          //Slider
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: GestureDetector(
+                  child: Image.asset("assets/images/executar.png"),
+                  onTap: (){
+                    _play();
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: GestureDetector(
+                  child: Image.asset("assets/images/pausar.png"),
+                  onTap: (){
+                    _pause();
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: GestureDetector(
+                  child: Image.asset("assets/images/parar.png"),
+                  onTap: (){
+                    _stop();
+                  },
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
